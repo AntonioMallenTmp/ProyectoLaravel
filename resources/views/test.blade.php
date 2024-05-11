df8e102b7073a0164c69780634b309d92843e4f1
AntonioMallen <antoniomallen1@gmail.com>
2024-05-11 14:13:01 +0200

    Se ha añadido Breeze al proyecto para el login

---
+++ resources/views/test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+<body>
+    <nav>
+        <a href="index" >Home</a>
+        <a href="test" >Alumnos</a>
+        <a href="" >Contacta</a>
+        <a href="" >About</a>
+    </nav>
+    <h1>Test</h1>
+    <hr>
+    <h2>Esto es test</h2>
+</body>
+</html>
